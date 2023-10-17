2368cb50af521e8435469de700ae4468dcfb4a2a
theo <jeangho293@naver.com>
2023-10-17 11:52:13 +0900

    feat: ecs version & package version up

--- src/ecsTransformer.ts
+++ src/ecsTransformer.ts
@@ -70,7 +70,7 @@ export const ecsTransformer = (
     "log.level": level,
     message,
     ecs: {
-      version: "8.5.0", // 로깅 필드를 변경할 경우 ecs version 에 맞춰야 한다.
+      version: "8.10.0", // 로깅 필드를 변경할 경우 ecs version 에 맞춰야 한다.
     },
     tags: ["request"],
     service: {
